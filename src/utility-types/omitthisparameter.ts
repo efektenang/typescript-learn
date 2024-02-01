a3b93884dab610e3f9e30426d69590c69675fa4f
Muhammad Arizky <78127662+efektenang@users.noreply.github.com>
2024-02-01 16:04:51 +0700

    Update omitthisparameter.ts

--- src/utility-types/omitthisparameter.ts
+++ src/utility-types/omitthisparameter.ts
@@ -1,13 +1,7 @@
-// type OriginalFunctionType = (this: { message: string }, name: string) => void; 
-  
-// type NewFunctionType = OmitThisParameter<OriginalFunctionType>; 
-  
 function toHex(this: Number) { 
     return this.toString(16)
 }; 
 
 const fiveToHex: OmitThisParameter<typeof toHex> = toHex.bind(5)
 
-  
-// Outputs: "Hello, GeeksforGeeks!" 
 console.log(fiveToHex())
